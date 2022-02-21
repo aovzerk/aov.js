7abe0c4162e4efb3b3ed305237a63858ff3c118f
aovzerk <pikseloooooo@gmail.com>
2022-02-21 21:17:36 +0400

    Update GUILD_MEMBER_UPDATE.js

--- src/client/actions/GUILD_MEMBER_UPDATE.js
+++ src/client/actions/GUILD_MEMBER_UPDATE.js
@@ -5,7 +5,7 @@ module.exports = async (client, action) => {
 
 	const guild = client.guilds.resolve(action.d.guild_id);
 	const new_member = new GuildMember(action.d, client);
-	const old_member = guild.members.resolve(new_member);
+	const old_member = guild.members.resolve(new_member.d.user.id);
 	Object.assign(new_member, old_member);
 	new_member.d = action.d;
 	guild.members.add(new_member);
