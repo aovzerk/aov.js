a8e65e0d9d4ccafcf08a09c23853335613054b8b
aovzerk <pikseloooooo@gmail.com>
2022-02-21 10:55:12 +0400

    Update BaseRestManager.js

--- src/client/managers/BaseRestManager.js
+++ src/client/managers/BaseRestManager.js
@@ -7,25 +7,27 @@ class BaseRestManager {
 		const { method, body, url, token } = options;
 		let { get_json } = options;
 		if (get_json == undefined) get_json = true;
-		return new Promise(async (result, reject) => {
-			const response = await fetch(url, {
+		return new Promise((result, reject) => {
+			fetch(url, {
 				"method": method,
 				"body": body,
 				"headers": {
 					"Authorization": `Bot ${token}`,
 					"Content-Type": "application/json"
 				}
-			});
-			if (response.status == API_STATUS.OK || response.status == API_STATUS.NOT_CONTENT || response.status == API_STATUS.CREATED) {
-				if (get_json) {
-					const data = await response.json();
-					result(data);
+			}).then(async response => {
+				if (response.status == API_STATUS.OK || response.status == API_STATUS.NOT_CONTENT || response.status == API_STATUS.CREATED) {
+					if (get_json) {
+						const data = await response.json();
+						result(data);
+					} else {
+						result("Complete");
+					}
 				} else {
-					result("Complete");
+					reject({ "Error_status": response.status, "Error_url": response.url, "Error_text": response.statusText, "body": JSON.parse(body) });
 				}
-			} else {
-				reject({ "Error_status": response.status, "Error_url": response.url, "Error_text": response.statusText, "body": JSON.parse(body) });
-			}
+			}).catch(err => reject(err));
+
 		});
 
 	}
